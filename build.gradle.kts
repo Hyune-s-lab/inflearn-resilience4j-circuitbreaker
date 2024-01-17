3004ed673195853adaa179bbda0c19acfef9d23a
Hyune-c <choi8608@gmail.com>
2024-01-17 14:04:14 +0900

    add dependency: web starter

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ extra["springCloudVersion"] = "2023.0.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
